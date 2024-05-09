a8cfcbbcd81162885f40eed28d4bbe4a86cd12b7
JackRipper01 <francohernandezpiloto@gmail.com>
2024-05-09 12:02:08 -0400

    enemy_movement

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
 @onready var inmunity_timer = $Inmunity_timer
+@onready var timer = $Timer
 
 func _ready():
 	inmunity_timer.start()
 func _on_body_entered(body):
 	if inmunity_timer.is_stopped():
 		print("Yu dai")
-		get_tree().reload_current_scene()
+		timer.start()
+		body.get_node("CollisionShape2D").queue_free()
+		Engine.time_scale = 0.5
+		
 
-
-
+func _on_timer_timeout_TimerNode():
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
