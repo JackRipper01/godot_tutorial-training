51a46721b0d6fd36dd17d98f59b765cb68739930
JackRipper01 <francohernandezpiloto@gmail.com>
2024-05-09 17:31:15 -0400

    COMPLETED BASICS

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-
+@onready var game_manager = %GameManager
+@onready var score_label = %Score_label
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
-	print("1+ coin")
-	queue_free()
+	game_manager.add_point()
+	score_label.text = "Coins: "+ str(game_manager.score)
+	animation_player.play("pickup")
